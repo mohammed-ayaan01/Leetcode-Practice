a61001d5867987b2f609fa19c3434a4809445a82
Mohammed Ayaan <mohammedayaan.official01@gmail.com>
2026-07-28 14:18:31 +0530

    Format #1421

--- 1421-find-numbers-with-even-number-of-digits/1421-find-numbers-with-even-number-of-digits.java
+++ 1421-find-numbers-with-even-number-of-digits/1421-find-numbers-with-even-number-of-digits.java
@@ -3,7 +3,11 @@
  * Link: leetcode.com/problems/find-numbers-with-even-number-of-digits/
  *
  * Approach:
- * Math and Arrays
+ * Digit Counting using Repeated Division
+ *
+ * Traverse each number in the array.
+ * Count the number of digits by repeatedly dividing by 10.
+ * If the digit count is even, increment the result.
  *
  * Language Used: Java
  *
@@ -15,7 +19,8 @@ class Solution {
     public int findNumbers(int[] nums) {
             int digit = 0;
         for(int i = 0;i < nums.length;i++){
-        int count = 0;
+            int count = 0; // Inside in a for loop because at every iteration of a number in array 
+            // count is set to 0 
             int temp = nums[i];
             while(temp > 0){ 
                 temp = temp / 10; //Remove the Last Element
