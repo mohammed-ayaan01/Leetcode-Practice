6cdc65a10f959155d9cf2c2581c9141a4ebf0b2e
Mohammed Ayaan <mohammedayaan.official01@gmail.com>
2026-07-29 05:26:33 +0530

    Add Time and Space Complexity

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,3 +1,16 @@
+/**
+ * Problem: Two Sum / #0001
+ * Link: https://leetcode.com/problems/two-sum/description/
+ *
+ * Approach:
+ * Brute Force Approach is Used Where we are traversing and scanning each element
+ *
+ * Language Used: Java
+ *
+ * Time Complexity: O(n^2)
+ * Space Complexity: O(1)
+ */
+
 class Solution {
     public int[] twoSum(int[] nums, int target) {
         
