a69f4faf9adf4fd5b673611ef305cd3083f62a67
Mohammed Ayaan <mohammedayaan.official01@gmail.com>
2026-08-19 14:15:22 +0530

    0001-TWO-SUM Stats: Time: 44 ms (35.25%), Space: 47 MB (74.78%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,29 +1,13 @@
-/**
- * Problem: Two Sum / #0001
- * Link: https://leetcode.com/problems/two-sum/description/
- *
- * Approach:
- * Brute Force Approach is Used Where we are traversing and scanning each element
- *
- * Language Used: Java
- *
- * Time Complexity: O(n^2)
- * Space Complexity: O(1)
- */
-
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        
-        int n = nums.length;
-        for(int i = 0; i < n;i++){
-            for(int j = i + 1;j < n;j++){
+        for(int i = 0;i < nums.length;i++){
+            for(int j = i + 1;j < nums.length;j++){
+
                 if(nums[i] + nums[j] == target){
                     return new int[]{i,j};
-
                 }
             }
         }
         return nums;
     }
-
 }
