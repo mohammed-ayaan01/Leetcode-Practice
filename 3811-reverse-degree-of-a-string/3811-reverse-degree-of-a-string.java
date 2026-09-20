6181d16c4f4fd9bb92a599297b2132b7931c5074
Mohammed Ayaan <mohammedayaan.official01@gmail.com>
2026-09-20 08:51:00 +0530

    3811-REVERSE-DEGREE-OF-A-STRING Stats: Time: 1 ms (100%), Space: 44.3 MB (10.65%) - LeetHub

---
+++ 3811-reverse-degree-of-a-string/3811-reverse-degree-of-a-string.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int reverseDegree(String s) {
+        int sum = 0;
+        for(int i = 0;i < s.length();i++){
+            int temp = 'z' - s.charAt(i) + 1;
+            int prod = temp * (i + 1);
+
+            sum+= prod;
+
+        }
+        return sum;
+    }
+}
